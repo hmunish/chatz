96e93d059d6a0a416da59912c771eecd3d2d0453
Munish <hmunish5@gmail.com>
2023-10-04 01:33:21 +0200

    Fix eslint errors

--- app/app.js
+++ app/app.js
@@ -1,21 +1,21 @@
-const path = require("path");
+const path = require('path');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 
-const sequelize = require("./utility/database.js");
+const sequelize = require('./utility/database.js');
 
 const app = express();
 
 // Serving pages for public router
-app.use("/public", (req, res) => {
+app.use('/public', (req, res) => {
   res.sendFile(path.join(__dirname, `../public${req.url}`));
 });
 
 // Responding with 404 not found error if no route matched by the request
-app.use("/", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "../public/404.html"));
+app.use('/', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '../public/404.html'));
 });
 
 sequelize
