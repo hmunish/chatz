c12db25239c2b76bb16b724dd625269049654743
Munish <hmunish5@gmail.com>
2023-10-04 01:30:28 +0200

    Fix eslint errors

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const express = require("express");
 
-const sequelize = require("./utility/database");
+const sequelize = require("./utility/database.js");
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use("/public", (req, res) => {
   res.sendFile(path.join(__dirname, `../public${req.url}`));
 });
 
-// 404 not found error
+// Responding with 404 not found error if no route matched by the request
 app.use("/", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "../public/404.html"));
 });
--- app/utility/database.js
+++ app/utility/database.js
@@ -1,13 +1,13 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    dialect: "mysql",
+    dialect: 'mysql',
     host: process.env.DB_HOSTNAME,
-  }
+  },
 );
 
 module.exports = sequelize;
