20eff596e6dec727c4114b195688d807409fca0f
Munish <hmunish5@gmail.com>
2023-10-04 01:29:27 +0200

    Add configuration for MySQL database connection

--- app/app.js
+++ app/app.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const path = require("path");
+
+require("dotenv").config();
+
+const express = require("express");
+
+const sequelize = require("./utility/database");
 
 const app = express();
 
-require('dotenv').config();
-
-app.use('/', (req, res) => {
-  res.send('recieved');
+// Serving pages for public router
+app.use("/public", (req, res) => {
+  res.sendFile(path.join(__dirname, `../public${req.url}`));
 });
 
-app.listen(3000);
+// 404 not found error
+app.use("/", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "../public/404.html"));
+});
+
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
