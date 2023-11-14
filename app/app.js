0eeb3c5d2488cb17bb03a6fcda8da927eea35b57
Munish <hmunish5@gmail.com>
2023-11-14 23:06:55 +0200

    Update for setting for aws-deployment

--- app/app.js
+++ app/app.js
@@ -1,31 +1,33 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const http = require("http");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 
-const io = new Server(5000, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(5000, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-const bodyParser = require("body-parser");
-const userRouter = require("./routes/user.js");
-const chatRouter = require("./routes/chat.js");
-const groupChatRouter = require("./routes/group-chat.js");
+const io = new Server(server);
 
-io.on("connection", (socket) => {
-  socket.on("join-group", (groupId) => {
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/user.js');
+const chatRouter = require('./routes/chat.js');
+const groupChatRouter = require('./routes/group-chat.js');
+
+io.on('connection', (socket) => {
+  socket.on('join-group', (groupId) => {
     socket.join(groupId);
   });
 });
@@ -37,69 +39,69 @@ app.use((req, res, next) => {
 });
 
 // Serving static files from dist folder
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
 // Using cors package to communicate between cross origin requests
 // And allowing requests only from the application url
-app.use(cors({ origin: process.env.HOSTNAME, methods: ["GET", "POST"] }));
+app.use(cors({ origin: process.env.HOSTNAME, methods: ['GET', 'POST'] }));
 
 // Using body parser to parse incoming json body
 app.use(bodyParser.json());
 
 // create a write stream for error logs (in append mode)
 const errorLogStream = fs.createWriteStream(
-  path.join(__dirname, "logs/error.log"),
+  path.join(__dirname, 'logs/error.log'),
   {
-    flags: "a",
-  }
+    flags: 'a',
+  },
 );
 
 // setup the logger for error
 app.use(
-  morgan("combined", {
+  morgan('combined', {
     skip(req, res) {
       return res.statusCode < 400;
     },
     stream: errorLogStream,
-  })
+  }),
 );
 
 // create a write stream for success logs (in append mode)
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "logs/access.log"),
+  path.join(__dirname, 'logs/access.log'),
   {
-    flags: "a",
-  }
+    flags: 'a',
+  },
 );
 
 // setup the logger for success logs
 app.use(
-  morgan("combined", {
+  morgan('combined', {
     skip(req, res) {
       return res.statusCode > 400;
     },
     stream: accessLogStream,
-  })
+  }),
 );
 
 // Users router
-app.use("/users", userRouter);
+app.use('/users', userRouter);
 
 // Chats router
-app.use("/chats", chatRouter);
+app.use('/chats', chatRouter);
 
 // Groups router
-app.use("/groups", groupChatRouter);
+app.use('/groups', groupChatRouter);
 
 // Responding with 404 not found error if no route matched by the request
-app.use("/", (req, res) => {
-  res.status(404).redirect("404.html");
+app.use('/', (req, res) => {
+  res.status(404).redirect('404.html');
 });
 
 // Setting up database connection with MongoDB server using mongoose ORM
 mongoose
   .connect(process.env.MONGODB_HOSTNAME)
   .then((res) => {
-    server.listen(process.env.PORT || 3000);
+    server.listen(80);
   })
   .catch((err) => {});
